4111667acc5af3cbaf7a8b3e01780c21473c02f2
Mainak Podder <mainakshuvo7@gmail.com>
2026-02-15 22:11:40 +0530

    Day 005: [Arrays][Expert] - [3 Sum][Better2][Best]

--- arrays/expert/ThreeSum.java
+++ arrays/expert/ThreeSum.java
@@ -1,14 +1,7 @@
 /*
-Problem: 
-- Given an array of N integers, your task is to find unique triplets that add up to give a sum of zero. In short, you need to return an array of all the unique triplets [arr[a], arr[b], arr[c]] such that i!=j, j!=k, k!=i, and their sum is equal to zero.
-
 Link:
 - https://takeuforward.org/data-structure/3-sum-find-triplets-that-add-up-to-a-zero
 - https://dsa.apnacollege.in/3sum
-
-Note:
-- Better1: By fixing one element and using two pointers to find the other two numbers that sum to the negative of the fixed element, we can efficiently find triplets.
-- Better2: For better approach earlier, we used three loops to find triplets that sum to zero. But now, we aim to do the same using just two loops. To do this, we will calculate the third number needed to complete the triplet instead of looping to find it.
 */
 
 import java.util.*;
@@ -16,6 +9,7 @@ import java.util.*;
 class ThreeSum {
     // TC: O(N^3*(logK)) -> k is no. of unique triplets
     // SC: O(K)
+    // Using Nested Loops
     public static List<List<Integer>> threeSum_BruteForce(int[] arr) {
         List<List<Integer>> result = new ArrayList<>();
         Set<List<Integer>> set = new HashSet<>();
@@ -39,7 +33,7 @@ class ThreeSum {
         return result;
     }
 
-    // TC: O(N^3*(logK)) -> k is no. of unique triplets
+    // TC: O(N^2*(logK)) -> k is no. of unique triplets
     // SC: O(K)
     // Using Two-Pointer and Set
     public static List<List<Integer>> threeSum_Better1(int[] arr) {
@@ -75,6 +69,80 @@ class ThreeSum {
         return result;
     }
 
+    // TC: O(N^2*(logK)) -> k is no. of unique triplets
+    // SC: O(K)
+    // Using Set
+    public static List<List<Integer>> threeSum_Better2(int[] arr) {
+        int length = arr.length;
+        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> uniqueTriplets = new HashSet<>();
+
+        for (int i = 0; i < length; i++) {
+            Set<Integer> seenNumbers = new HashSet<>();
+            for (int j = i + 1; j < length; j++) {
+                int firstNum = arr[i];
+                int secondNum = arr[j];
+                int requiredNum = -(firstNum + secondNum);
+
+                if (seenNumbers.contains(requiredNum)) {
+                    List<Integer> triplet = Arrays.asList(firstNum, secondNum, requiredNum);
+                    Collections.sort(triplet); // ensure consistent order
+                    uniqueTriplets.add(triplet);
+                }
+
+                seenNumbers.add(secondNum);
+            }
+        }
+
+        result.addAll(uniqueTriplets);
+        return result;
+    }
+
+    // TC: O(N^2)
+    // SC: O(1)
+    // Using Two-Pointer and Set
+    public static List<List<Integer>> threeSum_Best(int[] arr) {
+        int length = arr.length;
+        List<List<Integer>> triplets = new ArrayList<>();
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < length; i++) {
+            // [-4, -1, -1, 0, 1, 2] -> skip 2nd -1
+            if (i > 0 && arr[i] == arr[i - 1])
+                continue;
+
+            int targetSum = -arr[i];
+            int leftPointer = i + 1;
+            int rightPointer = length - 1;
+
+            while (leftPointer < rightPointer) {
+                int currentSum = arr[leftPointer] + arr[rightPointer];
+
+                if (currentSum == targetSum) {
+                    triplets.add(Arrays.asList(arr[i], arr[leftPointer], arr[rightPointer]));
+
+                    // Skip duplicate values
+                    if (leftPointer < rightPointer && arr[leftPointer] == arr[leftPointer + 1])
+                        leftPointer++;
+                    if (leftPointer < rightPointer && arr[rightPointer] == arr[rightPointer - 1])
+                        rightPointer++;
+
+                    leftPointer++;
+                    rightPointer--;
+                } else if (currentSum < 0) {
+                    // If sum is too small, move leftPointer forward to increase sum
+                    leftPointer++;
+                } else {
+                    // If sum is too large, move rightPointer backward to decrease sum
+                    rightPointer--;
+                }
+            }
+        }
+
+        return triplets;
+    }
+
     public static void printResult(List<List<Integer>> result) {
         for (List<Integer> triplet : result) {
             System.out.println(triplet);
@@ -85,7 +153,9 @@ class ThreeSum {
         int[] arr = { -1, 0, 1, 2, -1, -4 };
 
         // List<List<Integer>> result = threeSum_BruteForce(arr);
-        List<List<Integer>> result = threeSum_Better1(arr);
+        // List<List<Integer>> result = threeSum_Better1(arr);
+        List<List<Integer>> result = threeSum_Better2(arr);
+        // List<List<Integer>> result = threeSum_Best(arr);
 
         printResult(result);
     }
